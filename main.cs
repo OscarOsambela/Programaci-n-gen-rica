dc6c6d2dd27f1ba8b803114e063498d366929456
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-11-20 03:32:51 +0000

    add

--- main.cs
+++ main.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-
-class MainClass {
-  public static void Main (string[] args) {
+/*
+programacion generica sirve como reutilizacion de codigo, consiste crear una especie de clase comodin con el que se va a poder manejar 
+cualquier tipo de objeto(string, date, obj, etc)
+A la hora de instanciar la clase indicar entre parentesis angulares el tipo de objeto a manejar
+EjemploClase<String>archivos=new EjemploClase<String>();
+Se puede utilizar una clase que sirva como generica utilizandola herencia, sin embargo tiene sus inconvenientes:
+uso continuo del casting
+complicacion de codigo
+no posibilidad de comprobacion de errores 
+*/
+public class Program
+{
+  public static void Main()
+  {
 
   }
 }
